6137f4cae31809e1600ae3df89892262417cb1a8
Dragos <Dragos.Dragos@honeywell.com>
2019-07-06 20:29:22 +0300

    test

--- client/src/components/pages/add-icecream.js
+++ client/src/components/pages/add-icecream.js
@@ -4,6 +4,14 @@ export default class AddIcecream extends Component {
     constructor(props) {
         super(props);
 
+
+        this.onChangeFlavour = this.onChangeFlavour.bind(this);
+        this.onChangePrice = this.onChangePrice.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeQuantity = this.onChangeQuantity.bind(this);
+        this.onSubmit = this.bind(this);
+
+
         // set initial state
         this.state = {
             flavour: "",
@@ -11,6 +19,49 @@ export default class AddIcecream extends Component {
             description: "",
             quantity: ""
         }
+    }
+
+    // componentDidMount(){
+
+    // }
+
+    onChangeFlavour(e) {
+        this.setState({
+            flavour: e.target.value
+        });
+    }
+
+    onChangePrice(e) {
+        this.setState({
+            price: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeQuantity(e) {
+        this.setState({
+            quantity: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const icecream = {
+            flavour: this.state.flavour,
+            price: this.state.price,
+            description: this.state.description,
+            quantity: this.state.description
+        }
+
+        console.log(icecream);
+
+        window.location = "/";
     }
 
     render() {
